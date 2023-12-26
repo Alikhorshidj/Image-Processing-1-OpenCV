738e7b859762df881f5ee656aa0c22cd24003630
Alikhorshidj <121032889+Alikhorshidj@users.noreply.github.com>
2023-12-26 17:15:19 +0330

    5

--- Image-Processing/main.py
+++ Image-Processing/main.py
@@ -2,8 +2,95 @@ import cv2
 import numpy as np
 
 
-img = cv2.imread('')
+cap = cv2.VideoCapture(0)
 
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+while True :
+    ret, frame = cap.read()
+
+    out.write(frame)
+
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    cv2.imshow('webcam',img_gray)
+
+    # print(ret)
+
+    if cv2.waitKey(1) & 0xFF == ord('x'):
+        break
+
+out.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# img = cv2.imread('img/airplane2.jpg')
+#
+# flag = False
+# ix = -1
+# iy = -1
+#
+#
+# def crop(event, x, y, flags, param):
+#
+#     global flag,ix,iy
+#
+#     if event == 1:
+#         flag = True
+#         ix = x
+#         iy = y
+#
+#     # elif event == 0:
+#     #     if flag == True:
+#     #         cv2.rectangle(img, pt1=(ix,iy), pt2=(x,y),thickness=1,color=(0,0,0))
+#
+#     elif event == 4:
+#
+#         fx = x
+#         fy = y
+#
+#         flag = False
+#         cv2.rectangle(img, pt1=(ix, iy), pt2=(x, y), thickness=1, color=(0, 0, 0))
+#         # Crop tools
+#         cropped = img[iy:fy, ix:fx]
+#         cv2.imshow('new window', cropped)
+#         cv2.waitKey(0)
+#
+# cv2.namedWindow(winname="window")
+# cv2.setMouseCallback("window", crop)
+# while True:
+#
+#     cv2.imshow('window', img)
+#     if cv2.waitKey(1) & 0xFF == ord('x'):
+#         break
+#
+# cv2.destroyAllWindows()
 
 
 
