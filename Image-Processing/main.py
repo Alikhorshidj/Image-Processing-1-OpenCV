29a7d660f09d77151ab150e45428295c0dc24eae
Alikhorshidj <121032889+Alikhorshidj@users.noreply.github.com>
2023-12-26 09:12:59 +0330

    1

--- Image-Processing/main.py
+++ Image-Processing/main.py
@@ -1,5 +1,21 @@
 import cv2
 import numpy as np
 
+# Read an Image
+img = cv2.imread('img/fruits.jpg')
 
-  
+# img_gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+# print(img_gray.shape)
+
+
+# img[:, :, 0] = 0
+
+imgBlue = img[:, :, 0]
+imgGreen = img[:, :, 1]
+imgRed = img[:, :, 2]
+# new_img = np.hstack((img1, img2, img3))
+new_img = np.hstack((imgBlue, imgGreen,imgRed))
+
+cv2.imshow('window', new_img)
+cv2.waitKey(0)
